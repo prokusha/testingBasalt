93d94e06864eb0f6231427767c0b1d25d05182af
prokusha <prokudin216@gmail.com>
2024-07-05 13:05:17 +0300

    add install bssh script for lib

---
+++ lib/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PROJECT_DIR=$(dirname "$PWD/lib")
+
+BUILD_DIR="$PROJECT_DIR/build"
+
+if [ ! -d "$BUILD_DIR" ]; then
+    mkdir "$BUILD_DIR"
+fi
+
+cd "$BUILD_DIR"
+
+cmake "$PROJECT_DIR"
+
+cmake --build .
+
+sudo cmake --install .
